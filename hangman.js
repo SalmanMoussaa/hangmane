328cd4b30a88620a91c65158e4808abc1950f8eb
SalmanMoussaa <Salman.m.moussa@gmail.com>
2023-02-28 01:19:25 +0200

    creating the display update func

--- hangman.js
+++ hangman.js
@@ -18,6 +18,23 @@ window.onload= function(){
         guessesRemaining: 6,
         lettersGuessed: []
     };
+     // Initializing the display of the game state
+     updateDisplay();
 
-   
+    
+     function updateDisplay() {
+         var wordDisplay = "";
+         for (var i = 0; i < gameState.word.length; i++) {
+             if (gameState.lettersGuessed.indexOf(gameState.word[i]) !== -1) {
+                 wordDisplay += gameState.word[i];
+             } else {
+                 wordDisplay += "_";
+             }
+             wordDisplay += " ";
+         }
+         document.getElementById("word").textContent = wordDisplay;
+         document.getElementById("guesses").textContent = gameState.guessesRemaining;
+         document.getElementById("letters").textContent = gameState.lettersGuessed.join(", ");
+     }
+ 
 }
